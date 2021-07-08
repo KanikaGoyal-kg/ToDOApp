ccf155dabe2b7222d0b0b7a1e90e57540d40606e
Kanika Goyal <goyalkanu111@gmail.com>
2021-07-08 21:21:23 +0530

    made changes

--- src/interview.js
+++ src/interview.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 const interview = () => {
 
+    const arr = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4];
+
 
     return (
         <>
-        
+        <div>
+            {arr.filter((value, index) => {return arr.indexOf(value) === index} )}
+        </div>
         </>
     )
 };
