94268cbf7e48c724406cf8ac142f61ce6e1fbe58
Kanika Goyal <goyalkanu111@gmail.com>
2021-07-08 21:11:27 +0530

    made changes

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Todo from './component/Todo';
+import interview from './interview';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Todo />
+    <interview />
   </React.StrictMode>,
   document.getElementById('root')
 );
